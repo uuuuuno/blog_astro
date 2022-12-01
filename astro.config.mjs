34167e055d1ba39805ee1eab88e85da56fd847e0
uuuuuno <project@uuuuuno.ru>
2022-12-01 18:49:57 +0300

    edit site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://uno-blog.vercel.app/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
